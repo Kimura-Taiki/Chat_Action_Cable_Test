0ce7c4145ea71f9f0df73e665631fa56937aa599
Kimura-Taiki <taiguimucum850@gmail.com>
2022-03-19 15:10:26 +0000

    rails db:migrateでMessageデータベースを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_18_132129) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_19_150108) do
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "context", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
