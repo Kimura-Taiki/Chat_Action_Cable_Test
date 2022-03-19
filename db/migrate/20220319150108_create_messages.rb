3b25187879fe6c1d81ab7c12eca03a6b174923fa
Kimura-Taiki <taiguimucum850@gmail.com>
2022-03-19 15:05:18 +0000

    念の為にnull: falseを追加

--- db/migrate/20220319150108_create_messages.rb
+++ db/migrate/20220319150108_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
-      t.integer :user_id
-      t.text :context
+      t.integer :user_id, null: false
+      t.text :context, null: false
 
       t.timestamps
     end
