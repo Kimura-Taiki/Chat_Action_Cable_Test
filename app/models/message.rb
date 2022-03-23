cd4de6bdeaa0504706de753ed01612ae5ca17429
Kimura-Taiki <taiguimucum850@gmail.com>
2022-03-23 13:52:05 +0000

    動作確認のための seeds.rb を作成

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 500 }
+  validates :context, presence: true, length: { maximum: 500 }
 end
