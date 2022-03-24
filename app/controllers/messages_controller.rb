40aebc59e362d62b95305b9a577513f53b523723
Kimura-Taiki <taiguimucum850@gmail.com>
2022-03-24 11:43:40 +0000

    app/javascript/application.jsのコンソールでエラーを吐く行をコメントアウトした

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
   def create
+    @message = current_user.messages.create!(message_params)
+    p @message
+    # redirect_to "/"
+    # render
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:context)
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
+// この下の行が常にエラーを吐く・・・　なのでBootstrapも効くようなので消しておいた
+// import * as bootstrap from "bootstrap"
